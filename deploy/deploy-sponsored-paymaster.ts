f2c6daf092b00751582eec315386f235d0d93644
gbirckan <gbirckan@gmail.com>
2024-07-27 17:28:33 -0300

    feat: add cronos zkevm testnet to hardhat config

---
+++ deploy/deploy-sponsored-paymaster.ts
@@ -0,0 +1,28 @@
+import { Contract, Provider, Wallet, utils } from "zksync-ethers";
+import { Address } from "zksync-ethers/build/src/types";
+import { deployContract, fundAccount, getWallet } from "./prodUtils";
+
+export default async function () {
+    // Mainnet
+    // const verifierAddress: Address = "0xe65D3C2A9C43985763515Dd92099a4c63B1e108C";
+
+    // Testnet
+    const verifierAddress: Address = "0xa5A40aBBb41Ecb9379fE4E19Fcbc1788B8bFdE59";
+
+    // const AdminAddress: Address = "0xeacA6dB0aEe62c87a69C3d6Bcf6BCcc9388b7565";
+
+    require("dotenv").config();
+
+    const paymaster = await deployContract("ERC20SponsorPaymaster", [verifierAddress]);
+
+    const vault = await deployContract("SponsorshipVault", [paymaster.address]);
+
+    console.log("Setting vault in paymaster...");
+    const tx = await paymaster.setVault(vault.address);
+    console.log("Vault set in paymaster: ", tx.hash);
+
+    // Fund the paymaster
+    // await fundAccount(getWallet(), paymaster.address, "0.5");
+
+    // await paymaster.transferOwnership(AdminAddress);
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+import "@matterlabs/hardhat-zksync-ethers";
 import "@matterlabs/hardhat-zksync-chai-matchers";
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-node";
@@ -8,55 +9,59 @@ import "@nomicfoundation/hardhat-chai-matchers";
 import type { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "zkSyncSepoliaTestnet",
-  networks: {
-    zkSyncSepoliaTestnet: {
-      url: "https://sepolia.era.zksync.dev",
-      ethNetwork: "sepolia",
-      zksync: true,
-      verifyURL:
-        "https://explorer.sepolia.era.zksync.dev/contract_verification",
-      // "https://sepolia-era.zksync.network"
+    defaultNetwork: "zkSyncSepoliaTestnet",
+    networks: {
+        zkSyncSepoliaTestnet: {
+            url: "https://sepolia.era.zksync.dev",
+            ethNetwork: "sepolia",
+            zksync: true,
+            verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
+            // "https://sepolia-era.zksync.network"
+        },
+        zkSyncMainnet: {
+            url: "https://mainnet.era.zksync.io",
+            ethNetwork: "mainnet",
+            zksync: true,
+            verifyURL:
+                // "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+                "https://explorer.zksync.io/",
+        },
+        cronosZkEvmTestnet: {
+            url: "https://testnet.zkevm.cronos.org",
+            ethNetwork: "",
+            zksync: true,
+        },
+        dockerizedNode: {
+            url: "http://localhost:3050",
+            ethNetwork: "http://localhost:8545",
+            zksync: true,
+        },
+        inMemoryNode: {
+            url: "http://127.0.0.1:8011",
+            ethNetwork: "", // in-memory node doesn't support eth node; removing this line will cause an error
+            zksync: true,
+            // verifyURL: "http://localhost:3010/contract_verification",
+        },
+        hardhat: {
+            zksync: true,
+        },
     },
-    zkSyncMainnet: {
-      url: "https://mainnet.era.zksync.io",
-      ethNetwork: "mainnet",
-      zksync: true,
-      verifyURL:
-        // "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
-        "https://explorer.zksync.io/",
+    zksolc: {
+        version: "latest",
+        settings: {
+            // find all available options in the official documentation
+            // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
+        },
     },
-    dockerizedNode: {
-      url: "http://localhost:3050",
-      ethNetwork: "http://localhost:8545",
-      zksync: true,
+    solidity: {
+        version: "0.8.19",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
     },
-    inMemoryNode: {
-      url: "http://127.0.0.1:8011",
-      ethNetwork: "", // in-memory node doesn't support eth node; removing this line will cause an error
-      zksync: true,
-      // verifyURL: "http://localhost:3010/contract_verification",
-    },
-    hardhat: {
-      zksync: true,
-    },
-  },
-  zksolc: {
-    version: "latest",
-    settings: {
-      // find all available options in the official documentation
-      // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
-    },
-  },
-  solidity: {
-    version: "0.8.19",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
 };
 
 export default config;
