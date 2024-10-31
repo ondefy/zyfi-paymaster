23bc2634c50079537ab2d0093ad4afa4f2e9efeb
hoshiyari <hoshiyari420@gmail.com>
2024-10-31 23:50:02 +0530

    chore: add error signature comments

--- contracts/libraries/Errors.sol
+++ contracts/libraries/Errors.sol
@@ -5,24 +5,24 @@ library Errors {
                               PAYMASTER
     //////////////////////////////////////////////////////////////*/
 
-    error NotFromBootloader();
-    error ShortPaymasterInput();
-    error UnsupportedPaymasterFlow();
-    error TransactionExpired();
-    error InvalidAddress();
-    error InvalidMarkup();
-    error InvalidNonce();
-    error InvalidRatio();
-    error AllowanceTooLow();
-    error FailedTransferToBootloader();
-    error FailedTransfer();
-    error ArraysLengthMismatch();
+    error NotFromBootloader(); // 0x0e7b35b3
+    error ShortPaymasterInput(); // 0x1a539700
+    error UnsupportedPaymasterFlow(); // 0xff15b069
+    error TransactionExpired(); // 0xe397952c
+    error InvalidAddress(); // 0xe6c4247b
+    error InvalidMarkup(); // 0x99c36539
+    error InvalidNonce(); // 0x756688fe
+    error InvalidRatio(); // 0x648564d3
+    error AllowanceTooLow(); // 0xbac9b5e8
+    error FailedTransferToBootloader(); // 0x9e85dbaa
+    error FailedTransfer(); // 0xbfa871c5
+    error ArraysLengthMismatch(); // 0xfc235960
 
     /*//////////////////////////////////////////////////////////////
                               VAULT
     //////////////////////////////////////////////////////////////*/
-    error FailedTransferToPaymaster();
-    error FailedWithdrawal();
-    error NotEnoughFunds();
-    error NotFromPaymaster();
+    error FailedTransferToPaymaster(); // 0x6daa2717
+    error FailedWithdrawal(); // 0xa1248235
+    error NotEnoughFunds(); // 0x81b5ad68
+    error NotFromPaymaster(); // 0x975ba010
 }
